6d6022d512cff5ac9d74a1fc6bd1247fc3fcdde5
Chamandeep Singh <chamandeep617@gmail.com>
2021-02-27 17:41:02 +0530

    Lecture7 (Chat Application)

---
+++ Lecture7/chat_application/src/test/java/com/example/chat_application/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
