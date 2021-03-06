1670db4dd7375f891e715a8b8f0eadcf754e8e89
Chamandeep Singh <chamandeep617@gmail.com>
2021-03-06 12:15:17 +0530

    Lecture9 (library_management)

---
+++ Lecture9/library_management/src/main/java/com/example/library_management/Exception/BookNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.library_management.Exception;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(int id) {
+
+        super("Book id not found : " + id);
+    }
+
+    public BookNotFoundException() {
+
+        super("Book id not found : " );
+    }
+}
